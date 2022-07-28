de9196d75dda6fbe282fe99c7f3d5267c6d4babe
Mohammed Zakria Khan <mohammedzakriakhanz@gmai.com>
2022-07-28 12:31:15 +0530

    First Commit

---
+++ template.py
@@ -0,0 +1,120 @@
+import tkinter as tk
+
+def about_us_display():
+    A = tk.Tk()
+    A.title("Blood Group Selection")
+
+    def this_list1():
+        top = tk.Tk()
+
+        Lb1 = tk.Listbox(top)
+        Lb1.insert(1, "O-")
+        Lb1.grid(row=1, column=1)
+        top.mainloop()
+
+    def this_list2():
+        top = tk.Tk()
+
+        Lb1 = tk.Listbox(top)
+        Lb1.insert(1, "O-")
+        Lb1.insert(2, "O+")
+        Lb1.grid(row=1, column=1)
+        top.mainloop()
+
+    def this_list3():
+        top = tk.Tk()
+
+        Lb1 = tk.Listbox(top)
+        Lb1.insert(1, "O-")
+        Lb1.insert(2, "A-")
+
+        Lb1.grid(row=1, column=1)
+        top.mainloop()
+
+    def this_list4():
+        top = tk.Tk()
+
+        Lb1 = tk.Listbox(top)
+        Lb1.insert(1, "O-")
+        Lb1.insert(2, "O+")
+        Lb1.insert(3, "A-")
+        Lb1.insert(4, "A+")
+        Lb1.grid(row=1, column=1)
+        top.mainloop()
+
+
+    def this_list5():
+        top = tk.Tk()
+
+        Lb1 = tk.Listbox(top)
+        Lb1.insert(1, "O-")
+        Lb1.insert(3, "B-")
+
+        Lb1.grid(row=1, column=1)
+        top.mainloop()
+
+
+    def this_list6():
+        top = tk.Tk()
+
+        Lb1 = tk.Listbox(top)
+        Lb1.insert(1, "O-")
+        Lb1.insert(2, "O+")
+        Lb1.insert(3, "B-")
+        Lb1.insert(4, "B+")
+        Lb1.grid(row=1, column=1)
+        top.mainloop()
+
+
+    def this_list7():
+        top = tk.Tk()
+
+        Lb1 = tk.Listbox(top)
+        Lb1.insert(1, "O-")
+        Lb1.insert(2, "B-")
+        Lb1.insert(3, "A-")
+        Lb1.insert(4, "AB-")
+        Lb1.grid(row=1, column=1)
+        top.mainloop()
+
+
+    def this_list8():
+        top = tk.Tk()
+
+        Lb1 = tk.Listbox(top)
+        Lb1.insert(1, "O-")
+        Lb1.insert(2, "O+")
+        Lb1.insert(3, "A-")
+        Lb1.insert(4, "A+")
+        Lb1.insert(1, "B-")
+        Lb1.insert(1, "B+")
+        Lb1.insert(1, "AB-")
+        Lb1.insert(1, "AB+")
+
+        Lb1.grid(row=1, column=1)
+        top.mainloop()
+
+
+  
+    info_ = tk.Label(A, text="Click the blood group,you want blood for:")
+    g1 = tk.Button(A, text="O-", command=this_list1)
+    g2 = tk.Button(A, text="O+", command=this_list2)
+    g3 = tk.Button(A, text="A-", command=this_list3)
+    g4 = tk.Button(A, text="A+",command = this_list4)
+    g5 = tk.Button(A,text = 'B-',command = this_list5)
+    g6 = tk.Button(A,text = 'B+',command = this_list6)
+    g7 = tk.Button(A,text = 'AB-',command = this_list7)
+    g8 = tk.Button(A,text = 'AB-',command = this_list8)
+    g1.grid(row = 4,column = 1)
+    g2.grid(row=5, column=1)
+    g3.grid(row=6, column=1)
+    g4.grid(row=7, column=1)
+    g5.grid(row=8, column=1)
+    g6.grid(row=9, column=1)
+    g7.grid(row=10, column=1)
+    g8.grid(row=11, column=1)
+
+
+    #start_info.grid(row=1, column=1)
+    info_.grid(row=3, column=1)
+    A.mainloop()
